796026e90556ef974a3bc69aa2d7204ff5c6b24b
Reijjo <teemu.aitomeri@gmail.com>
2025-11-10 17:11:45 +0200

    added useragent header

--- server/src/routes/products.rs
+++ server/src/routes/products.rs
@@ -20,6 +20,7 @@ async fn get_products(
 ) -> Result<Json<Vec<Product>>, (StatusCode, Json<serde_json::Value>)> {
 	let response = match state.http_client
 		.get("https://fakestoreapi.com/products")
+		.header("User-Agent", "rust-axum-client")
 		.send()
 		.await
 	{
